ed202e91915e2daa81799ec4101627b915dd5e60
mobeets <mobeets@gmail.com>
2020-03-31 21:27:30 -0400

    added control panel

--- static/js/tiles.js
+++ static/js/tiles.js
@@ -21,7 +21,10 @@ let resourceColors = ['#f0340e', '#fcba03', '#272adb', '#a8329d', '#18b52f'];
 
 var canvas;
 var canvasWidth = grid_cols*col_width;
-var canvasHeight = grid_rows*row_height;
+var canvasHeight = grid_rows*row_height + 100;
+
+let button_draw_tile;
+let drawn_tile;
 
 function windowResized() {
   resizeCanvas(canvasWidth, canvasHeight);
@@ -33,24 +36,30 @@ function preload() {
 
 class Tile {
 
-  constructor(col, row){
+  constructor(col, row, isOnBoard){
     this.tile_id = -1;
     this.resource_id = -1;
     this.resource_corner = -1;
     this.col = col;
     this.row = row;
+    this.isOnBoard = isOnBoard;
+    if (isOnBoard) {
+      this.hidden = false;
+    } else {
+      this.hidden = true;
+    }
   }
 
   rotate() {
     for (let i = 0; i < baseTileIndices.length; i++) {
-        if (i === baseTileIndices.length-1 || this.tile_id < baseTileIndices[i+1]) {
-            // rotate the road tile
-            let j = this.tile_id-baseTileIndices[i];
-            this.tile_id = baseTileIndices[i] + ((j+1) % baseTileCounts[i]);
-            // rotate the corner the resource is in
-            this.resource_corner = (this.resource_corner+1) % 4;
-            return;
-        }
+      if (i === baseTileIndices.length-1 || this.tile_id < baseTileIndices[i+1]) {
+        // rotate the road tile
+        let j = this.tile_id-baseTileIndices[i];
+        this.tile_id = baseTileIndices[i] + ((j+1) % baseTileCounts[i]);
+        // rotate the corner the resource is in
+        this.resource_corner = (this.resource_corner+1) % 4;
+        return;
+      }
     }
   }
 
@@ -62,8 +71,15 @@ class Tile {
     }
 
     // find location to draw
-    let x = this.col * col_width;
-    let y = this.row * row_height;
+    let x, y;
+    if (this.isOnBoard) {
+      x = this.col * col_width;
+      y = this.row * row_height;
+    } else if (!this.hidden) {
+      // display in control panel
+      x = 1 * col_width;
+      y = (grid_rows+1) * row_height;
+    }
 
     // the tiles are packed into a single 4 x 4 atlas
     // we need calculate what part of the image to draw
@@ -73,18 +89,18 @@ class Tile {
     // draw it
     image(road_sprites, x, y, col_width, row_height, sx, sy, sprite_size, sprite_size);
     if (this.resource_id > -1) {
-        // textAlign(LEFT, TOP);
-        // text(res,x,y);
-        fill(resourceColors[this.resource_id]);
-        if (this.resource_corner === 0) {
-            circle(x+resourceDiameter, y+resourceDiameter, resourceDiameter);
-        } else if (this.resource_corner === 1) {
-            circle(x+col_width-resourceDiameter, y+resourceDiameter, resourceDiameter);
-        } else if (this.resource_corner === 2) {
-            circle(x+col_width-resourceDiameter, y+row_height-resourceDiameter, resourceDiameter);
-        } else {
-            circle(x+resourceDiameter, y+row_height-resourceDiameter, resourceDiameter);
-        }
+      // textAlign(LEFT, TOP);
+      // text(res,x,y);
+      fill(resourceColors[this.resource_id]);
+      if (this.resource_corner === 0) {
+        circle(x+resourceDiameter, y+resourceDiameter, resourceDiameter);
+      } else if (this.resource_corner === 1) {
+        circle(x+col_width-resourceDiameter, y+resourceDiameter, resourceDiameter);
+      } else if (this.resource_corner === 2) {
+        circle(x+col_width-resourceDiameter, y+row_height-resourceDiameter, resourceDiameter);
+      } else {
+        circle(x+resourceDiameter, y+row_height-resourceDiameter, resourceDiameter);
+      }
     }
   }
 
@@ -105,7 +121,6 @@ class Tile {
     this.tile_id = roadIndex;
     this.resource_id = roadResource;
     this.resource_corner = resourceCorner;
-    console.log(this);
   }
 }
 
@@ -115,12 +130,21 @@ function initializeTiles() {
   for (let col = 0; col < grid_cols; col++){
     tiles[col] = [];
     for (let row = 0; row < grid_rows; row++){
-      tiles[col][row] = new Tile(col, row);
+      tiles[col][row] = new Tile(col, row, true);
     }
   }
 
   // set HOME tile
   tiles[3][3].tile_id = tileCount-1;
+
+  // set DRAWN tile
+  drawn_tile = new Tile(0, 0, false);
+}
+
+function drawRandomTile() {
+  // draw a random tile and display it in the control panel
+  drawn_tile.randomize();
+  drawn_tile.hidden = false;
 }
 
 function setup() {
@@ -132,33 +156,51 @@ function setup() {
 
 function draw() {
   background('#8cc63e');
+  
+  // draw control panel
+  fill(255);
+  rect(0, grid_rows*row_height, grid_cols*col_width, 100);
+
   renderTiles();
   drawGridLines();
 }
 
 function mouseClicked() {
-  // find the grid location of the click
+  // check if a tile was clicked
   let col = floor(mouseX / col_width);
-  let row = floor(mouseY / col_width);
-  tiles[col][row].click();
+  let row = floor(mouseY / row_height);
+  if (col > 0 && row > 0 && col < tiles.length && row < tiles[col].length) {
+    tiles[col][row].click();
+  }
+
 }
 
 function renderTiles() {
   // loop over each cell
   for (let col = 0; col < grid_cols; col++) {
-      for (let row = 0; row < grid_rows; row++) {
-          tiles[col][row].render();
-      }
+    for (let row = 0; row < grid_rows; row++) {
+        tiles[col][row].render();
+    }
   }
+  drawn_tile.render();
 }
 
 function drawGridLines() {
   // draw grid lines
   stroke(100, 100, 100, 50);
-  for (let x = 0; x < width; x += col_width) {
-      line(x, 0, x, height);
+  for (let x = 0; x <= grid_cols*col_width; x += col_width) {
+    line(x, 0, x, grid_rows*row_height);
   }
-  for (let y = 0; y < height; y += row_height) {
-      line(0, y, width, y);
+  for (let y = 0; y <= grid_rows*row_height; y += row_height) {
+    line(0, y, grid_cols*col_width, y);
   }
 }
+
+
+function addHandlers() {
+  $("#draw-tile").click(drawRandomTile);
+}
+
+$(document).ready(function() {
+  addHandlers();
+});
